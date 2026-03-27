adaf82715bc6744195f844e27f94c3f2796fd4d5
Kanwal Reehal <Kanwal.Reehal@gmail.com>
2026-03-27 09:47:33 -0400

    Update streamlit_app.py
    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write('The name on your Smoothie will be:',name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
 # Convert the Snowflake Dataframe to Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_panda()
