b756cde06eeee54e6eda0a26c84fedfadf989c29
Kanwal Reehal <Kanwal.Reehal@gmail.com>
2026-03-27 08:37:44 -0400

    Update streamlit_app.py
    fixed the sf_df load statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ if ingredients_list:
 # Display the smoothiefroot nutrition information, using the APIs
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
